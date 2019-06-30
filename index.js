6c57187e0a6ee8784ec13296f0ce0071a26d52e5
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-30 13:25:41 -0400

    can't get it to import message-service.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import MessageService from "./message-service.js";
 let userId = "dandridge-cole";
 const messageService = new MessageService(userId);
 
-window.addEventListener("load", fucntion(){
-	document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
-	messageService.getAllMessages();
+window.addEventListener("load", function () {
+    document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
+    messageService.getAllMessages();
 });
--- message-service.js
+++ message-service.js
@@ -1,17 +1,19 @@
 export default class MessageService {
 
-	getAllMessages(){
-		let request = new XMLHttpRequest();
+	getAllMessages() {
+	    let request = new XMLHttpRequest();
 
-		request.onload = function() {
-			if (request.status >= 200 && request.status < 300) {
-            console.log(JSON.parse(request.responseText)); 
-        } else {
-            console.log('Error: ' + request.status); 
-        }
-		};
-        request.open("GET", "http://zipcode.rocks:8085/messages");
+	    // Setup our listener to process compeleted requests
+	    request.onload = function() {
+	        if (request.status >= 200 && request.status < 300) {
+	            console.log(JSON.parse(request.responseText)); // 'This is the returned text.'
+	        } else {
+	            console.log('Error: ' + request.status); // An error occurred during the request.
+	        }
+	    };
 
-        request.send();
+	    request.open("GET", "http://zipcode.rocks:8085/messages");
+
+	    request.send();
 	}
 }
